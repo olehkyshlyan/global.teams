ffdc99adbef887e55b577e64e03c593b8ea27490
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2020-12-10 16:53:15 +0200

    Index.php file | 2020.12.10

---
+++ index.php
@@ -0,0 +1,59 @@
+<?
+
+include_once "classes/line1.php";
+include_once "classes/line2.php";
+include_once "classes/line3.php";
+include_once "classes/line4.php";
+include_once "classes/line5.php";
+include_once "classes/line6.php";
+include_once "classes/line7.php";
+include_once "classes/line8.php";
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  
+  <title></title>
+  
+  <link rel="stylesheet" type="text/css" href="css/index.css" />
+	<link rel="stylesheet" type="text/css" href="css/line1.css" />
+	<link rel="stylesheet" type="text/css" href="css/line2.css" />
+	<link rel="stylesheet" type="text/css" href="css/line3.css" />
+	<link rel="stylesheet" type="text/css" href="css/line4.css" />
+	<link rel="stylesheet" type="text/css" href="css/line5.css" />
+	<link rel="stylesheet" type="text/css" href="css/line6.css" />
+	<link rel="stylesheet" type="text/css" href="css/line7.css" />
+	<link rel="stylesheet" type="text/css" href="css/line8.css" />
+  <style type="text/css">
+    
+  </style>
+  
+  <script type='text/javascript' src=''></script>
+  <script type="text/javascript">
+    
+  </script>
+</head>
+
+<body>
+
+<? print(Line1::$content); ?>
+<? print(Line2::$content); ?>
+<? print(Line3::$content); ?>
+<? print(Line4::$content); ?>
+<? print(Line5::$content); ?>
+<? print(Line6::$content); ?>
+<? print(Line7::$content); ?>
+<? print(Line8::$content); ?>
+
+</body>
+
+</html>
+
+<!--
+
+
+
+-->
