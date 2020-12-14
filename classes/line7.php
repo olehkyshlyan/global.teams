2b00cb05795b5cf39f0e766a65983a8c8ee1a6ef
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2020-12-14 11:43:20 +0200

    Update line7.php

--- classes/line7.php
+++ classes/line7.php
@@ -63,7 +63,7 @@ class Line7{
 	}
 	
 	private function creatingFifthTxtSection(){
-		$txtLine = '<div id="ln7-5thSecInputWrapper"><input id="" type="button" value="Play" /></div>';
+		$txtLine = '<div id="ln7-5thSecInputWrapper"><input id="ln7-5thSecInput" class="ln-5thSecInput" type="button" value="Play" /></div>';
 		self::$fifthTxtSection = '<div id="ln7-fifthTxtSection">'.$txtLine.'</div>';
 	}
 	
