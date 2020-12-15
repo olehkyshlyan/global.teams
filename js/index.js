6577f810133f6695df7cdbade3a35bee04b9a24e
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2020-12-15 18:51:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,17 @@
 
-var ActionInfo = new function(){
+var ActionsInfo = new function(){
 	
 	this.getInfo = function(){
 		jQuery(function(){
 			jQuery(".ln-5thSecInput").on('click',function(){
 				var buttonID = this.id;
-				alert('buttonID: '+buttonID);
 				jQuery.ajax({
 					method: "POST",
 					url: "classes/saveinfo.php",
 					data: {request: 'yes', buttonID: buttonID},
 					complete: function(jqXHR,textStatus){
-						//alert('textStatus: '+textStatus);
-						//alert('jqXHR: '+jqXHR);
 						alert('jqXHR.responseText: '+jqXHR.responseText);
 						var parsedResponseText = JSON.parse(jqXHR.responseText);
-						//alert('parsedResponseText: '+parsedResponseText);
-						//alert('parsedResponseText.errorInfo: '+parsedResponseText.errorInfo);
-						//alert('parsedResponseText.request: '+parsedResponseText.request);
 					}
 				});
 			});
@@ -26,4 +20,4 @@ var ActionInfo = new function(){
 	
 }
 
-ActionInfo.getInfo();
+ActionsInfo.getInfo();
